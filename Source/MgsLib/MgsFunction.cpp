0f6cf3052ceeb5845c7183fb80980a23c6a913b0
Paul <Paul@paul.com>
2016-02-21 12:50:08 +0000

    add a very basic way of snapshotting all vars and then comparing with another snapshot, useful for comparing reimpl vs real function depending on what it does (e.g if it deletes files, changes system state etc then forget it)

--- Source/MgsLib/MgsFunction.cpp
+++ Source/MgsLib/MgsFunction.cpp
@@ -2,6 +2,13 @@
 #include "MgsFunction.hpp"
 #include <set>
 
+struct TVarInfo
+{
+    DWORD mAddr;
+    DWORD mSize;
+    bool mIsPointerType;
+    bool mIsConstData;
+};
 
 // Since the MgsFunctionBase instances are global we have to do this to avoid global static init ordering issues
 /*static*/ std::map<DWORD, MgsFunctionBase*>& MgsFunctionBase::GetMgsFunctionTable()
@@ -9,14 +16,6 @@
     static std::map<DWORD, MgsFunctionBase*> table;
     return table;
 }
-
-
-// TODO: Check for multiply defined vars or overlapping vars
-struct TVarInfo
-{
-    DWORD mAddr;
-    DWORD mSize;
-};
 
 bool operator < (const TVarInfo& lhs, const TVarInfo& rhs)
 {
@@ -29,20 +28,195 @@ std::set<TVarInfo>& Vars()
     return v;
 }
 
-MgsVar::MgsVar(DWORD addr, DWORD sizeInBytes)
+MgsVar::MgsVar(DWORD addr, DWORD sizeInBytes, bool isPointerType, bool isConstData)
 {
     for (const auto& var : Vars())
     {
         if (var.mAddr == addr)
         {
             // Var has been defined twice
+            LOG_ERROR("Var at addr " << var.mAddr << " is defined more than once");
             abort();
         }
         else if (var.mAddr >= addr && var.mAddr + var.mSize <= addr)
         {
             // Var overlaps
+            LOG_ERROR("Var at addr " << var.mAddr << " overlaps another var");
             abort();
         }
     }
-    Vars().insert({ addr, sizeInBytes });
+    Vars().insert({ addr, sizeInBytes, isPointerType, isConstData });
+}
+
+/*static*/ void  MgsVar::TrackAlloc(void* ptr, size_t size)
+{
+    AutoCsLock lock(mCs);
+    mAllocs[ptr] = size;
+}
+
+/*static*/ void  MgsVar::TrackFree(void* ptr)
+{
+    AutoCsLock lock(mCs);
+    auto it = mAllocs.find(ptr);
+    if (it != std::end(mAllocs))
+    {
+        mAllocs.erase(it);
+    }
+}
+
+/*static*/ AutoCs MgsVar::mCs;
+/*static*/ std::map<void*, size_t> MgsVar::mAllocs;
+
+/*static*/ std::unique_ptr<MgsVar::SnapShot> MgsVar::MakeSnapShot()
+{
+    AutoCsLock lock(mCs);
+    return std::make_unique<MgsVar::SnapShot>();
+}
+
+MgsVar::SnapShot::SnapShot()
+{
+    // Take a copy of the vars at this point in time
+    for (const TVarInfo& v : Vars())
+    {
+        // Const data is read only, trying to write to it will attempt to write back the same value
+        // and crash trying to do. This could be improved to snapshot only and then have the restore
+        // assert it didn't change.
+        if (!v.mIsConstData)
+        {
+            if (v.mIsPointerType)
+            {
+                mVars[v.mAddr] = std::make_unique<DynamicVarSnapShot>(v.mAddr);
+            }
+            else
+            {
+                mVars[v.mAddr] = std::make_unique<StaticVarSnapShot>(v.mAddr, v.mSize);
+            }
+        }
+    }
+}
+
+void MgsVar::SnapShot::AssertEqual(const SnapShot& other)
+{
+    for (const auto& varSnap : mVars)
+    {
+        const auto& otherSnap = other.mVars.find(varSnap.second->Address());
+
+        auto tmpLhs = dynamic_cast<StaticVarSnapShot*>(varSnap.second.get());
+        if (tmpLhs)
+        {
+            // Compare static vars
+            auto lhs = dynamic_cast<StaticVarSnapShot*>(varSnap.second.get());
+            auto rhs = dynamic_cast<StaticVarSnapShot*>(otherSnap->second.get());
+            if (*lhs != *rhs)
+            {
+                abort();
+            }
+        }
+        else
+        {
+            // Compare dynamic vars
+            auto lhs = dynamic_cast<DynamicVarSnapShot*>(varSnap.second.get());
+            auto rhs = dynamic_cast<DynamicVarSnapShot*>(otherSnap->second.get());
+            if (*lhs != *rhs)
+            {
+                abort();
+            }
+        }
+    }
+}
+
+void MgsVar::SnapShot::Restore()
+{
+    for (auto& varSnap : mVars)
+    {
+        varSnap.second->Restore();
+    }
+}
+
+void StaticVarSnapShot::Restore()
+{
+    BYTE* value = ((BYTE*)mAddr);
+    memcpy(value, mValue.data(), mSize);
+}
+
+void StaticVarSnapShot::SnapShot()
+{
+    BYTE* value = ((BYTE*)mAddr);
+
+    mValue.resize(mSize);
+    memcpy(mValue.data(), value, mSize);
+}
+
+bool StaticVarSnapShot::operator == (const StaticVarSnapShot& other)
+{
+    if (mAddr != other.mAddr)
+    {
+        abort();
+    }
+
+    if (mSize != other.mSize)
+    {
+        abort();
+    }
+
+    if (other.mValue != mValue)
+    {
+        abort();
+    }
+
+    return true;
+}
+
+void DynamicVarSnapShot::Restore()
+{
+    void* pointerValue = ((DWORD*)mAddr);
+    if (DWORD(pointerValue) != mPtrValue)
+    {
+        LOG_WARNING("Pointer value changed, can't restore as original might have been freed!");
+    }
+    else
+    {
+        void* pointerValue = ((DWORD*)mAddr);
+        memcpy(pointerValue, mBufferCopy.data(), mBufferCopy.size());
+    }
+}
+
+void DynamicVarSnapShot::SnapShot()
+{
+    // Get the address
+    void* pointerValue = ((DWORD*)mAddr);
+
+    // See if its in the alloc map
+    auto it = MgsVar::mAllocs.find(pointerValue);
+
+    if (it != std::end(MgsVar::mAllocs))
+    {
+        // It is, copy the buffer that we point to
+        const size_t allocSize = it->second;
+        mBufferCopy.resize(allocSize);
+        memcpy(mBufferCopy.data(), pointerValue, allocSize);
+    }
+
+    // And always copy the pointer value itself
+    mPtrValue = (DWORD)pointerValue;
+}
+
+bool DynamicVarSnapShot::operator == (const DynamicVarSnapShot& other)
+{
+    if (mAddr != other.mAddr)
+    {
+        abort();
+    }
+
+    if (mPtrValue != other.mPtrValue)
+    {
+        abort();
+    }
+
+    if (mBufferCopy != other.mBufferCopy)
+    {
+        abort();
+    }
+
+    return true;
 }
