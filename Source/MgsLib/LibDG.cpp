fd684cbf5edb79ffd0c697b25e37e722fc1210a6
Paul <Paul@paul.com>
2017-08-28 11:17:11 +0100

    refactor and add LibDG_ClearActiveResourceFunctionPointerList_457B7C and LibDG_GetResourceInitFuncPtr_457BAC

--- Source/MgsLib/LibDG.cpp
+++ Source/MgsLib/LibDG.cpp
@@ -3,9 +3,10 @@
 
 void LibDGCppForceLink() { }
 
-MGS_ARY(1, 0x669AE0, Res_Init_Record, 512, gResInitFuncs_669AE0, {});
-MGS_ARY(1, 0x994320, Res_Init_Record, 512, gActiveResFuncs1_word_994320, {});
-MGS_PTR(1, 0x993F44, Res_Init_Record**, gpActiveResInitFuncs_dword_993F44, nullptr);
+MGS_ARY(1, 0x669AE0, Res_Init_Record, 512, gKnownResInitFuncs_669AE0, {});
+MGS_ARY(1, 0x994320, Res_Init_Record, 512, gDynamicResFuncs_word_994320, {});
+MGS_ARY(1, 0x650040, Res_Init_Record, 3, gStaticResInitFuncs_stru_650040, {});
+MGS_PTR(1, 0x993F44, Res_Init_Record**, gpToDynamicResInitFuncs_dword_993F44, nullptr);
 
 MGS_VAR(1, 0x994304, DWORD, gSleep5000_after_res_funcs_1_dword_994304, 0);
 MGS_VAR(1, 0x99430C, DWORD, gSleep5000_after_res_funcs_2_dword_99430C, 0);
@@ -15,31 +16,31 @@ signed int CC LibDG_CHARA_44E9D2(Res_Init_Record* pStartingRecord)
     int dst_idx = 0;
     for (;;)
     {
-        int src_idx = 0;
         if (!pStartingRecord->mInitFunctionPointer)
         {
             break;
         }
 
-        while (gResInitFuncs_669AE0[src_idx].mHashedName)
+        int src_idx = 0;
+        while (gKnownResInitFuncs_669AE0[src_idx].mHashedName)
         {
-            if (gResInitFuncs_669AE0[src_idx].mHashedName == pStartingRecord->mHashedName)
+            if (gKnownResInitFuncs_669AE0[src_idx].mHashedName == pStartingRecord->mHashedName)
             {
-                gActiveResFuncs1_word_994320[dst_idx].mHashedName = pStartingRecord->mHashedName;
-                gActiveResFuncs1_word_994320[dst_idx].mInitFunctionPointer = gResInitFuncs_669AE0[src_idx].mInitFunctionPointer;
+                gDynamicResFuncs_word_994320[dst_idx].mHashedName = pStartingRecord->mHashedName;
+                gDynamicResFuncs_word_994320[dst_idx].mInitFunctionPointer = gKnownResInitFuncs_669AE0[src_idx].mInitFunctionPointer;
                 break;
             }
             src_idx++;
         }
-        printf("CHARA %d, %p\n", gActiveResFuncs1_word_994320[dst_idx].mHashedName, gActiveResFuncs1_word_994320[dst_idx].mInitFunctionPointer);
+        printf("CHARA %d, %p\n", gDynamicResFuncs_word_994320[dst_idx].mHashedName, gDynamicResFuncs_word_994320[dst_idx].mInitFunctionPointer);
 
         ++dst_idx;
         ++pStartingRecord;
     }
 
     // Set last entry to 0 to terminate
-    gActiveResFuncs1_word_994320[dst_idx].mHashedName = 0;
-    gActiveResFuncs1_word_994320[dst_idx].mInitFunctionPointer = nullptr;
+    gDynamicResFuncs_word_994320[dst_idx].mHashedName = 0;
+    gDynamicResFuncs_word_994320[dst_idx].mInitFunctionPointer = nullptr;
     
     if (gSleep5000_after_res_funcs_1_dword_994304 || gSleep5000_after_res_funcs_2_dword_99430C)
     {
@@ -55,7 +56,43 @@ MSG_FUNC_IMPL(0x0044E9D2, LibDG_CHARA_44E9D2);
 
 void CC LibDG_SetActiveResourceInitFuncPtrs_457B5B()
 {
-    memcpy(gActiveResFuncs1_word_994320, gResInitFuncs_669AE0, 4096u); // 4096u = sizeof(gResInitFuncs_669AE0)
-    *gpActiveResInitFuncs_dword_993F44 = gActiveResFuncs1_word_994320;
+    memcpy(gDynamicResFuncs_word_994320, gKnownResInitFuncs_669AE0, 4096u); // 4096u = sizeof(gResInitFuncs_669AE0)
+    *gpToDynamicResInitFuncs_dword_993F44 = gDynamicResFuncs_word_994320;
 }
 MSG_FUNC_IMPL(0x457B5B, LibDG_SetActiveResourceInitFuncPtrs_457B5B);
+
+void CC LibDG_ClearActiveResourceFunctionPointerList_457B7C()
+{
+    memset(gDynamicResFuncs_word_994320, 0, 4096u); // 4096u = sizeof(gResInitFuncs_669AE0)
+    *gpToDynamicResInitFuncs_dword_993F44 = gDynamicResFuncs_word_994320;
+}
+MSG_FUNC_IMPL(0x457B7C, LibDG_ClearActiveResourceFunctionPointerList_457B7C);
+
+static ResInitFn FindFnPtrByHash(Res_Init_Record* arrayToSearch, WORD hashName)
+{
+    Res_Init_Record* pRecord = arrayToSearch;
+    while (pRecord->mInitFunctionPointer)
+    {
+        if (pRecord->mHashedName == hashName)
+        {
+            return pRecord->mInitFunctionPointer;
+        }
+        ++pRecord;
+    }
+    return nullptr;
+}
+
+ResInitFn CC LibDG_GetResourceInitFuncPtr_457BAC(WORD hashedName)
+{
+    // Search in the 3 "static" records
+    ResInitFn result = FindFnPtrByHash(gStaticResInitFuncs_stru_650040, hashedName);
+    if (result)
+    {
+        return result;
+    }
+
+    // Search in the 512 "dynamic" records
+    result = FindFnPtrByHash(*gpToDynamicResInitFuncs_dword_993F44, hashedName);
+    return result;
+}
+MSG_FUNC_IMPL(0x457BAC, LibDG_GetResourceInitFuncPtr_457BAC);
--- Source/MgsLib/LibDG.hpp
+++ Source/MgsLib/LibDG.hpp
@@ -4,13 +4,17 @@
 
 void LibDGCppForceLink();
 
+using ResInitFn = void*;
+
 struct Res_Init_Record
 {
     WORD mHashedName;
     WORD mPadding;
-    void* mInitFunctionPointer;
+    ResInitFn mInitFunctionPointer;
 };
 MSG_ASSERT_SIZEOF(Res_Init_Record, 0x8);
 
 signed int CC LibDG_CHARA_44E9D2(Res_Init_Record *pStartingRecord);
 void CC LibDG_SetActiveResourceInitFuncPtrs_457B5B();
+void CC LibDG_ClearActiveResourceFunctionPointerList_457B7C();
+ResInitFn CC LibDG_GetResourceInitFuncPtr_457BAC(WORD hashedName);
