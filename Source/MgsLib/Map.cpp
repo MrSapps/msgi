011d18b8a01641d94fb65f2b95980496eec64aaa
Paul <Paul@paul.com>
2018-04-21 00:17:53 +0100

    impl Map_LitLoad_44F53B

--- Source/MgsLib/Map.cpp
+++ Source/MgsLib/Map.cpp
@@ -6,8 +6,24 @@
 #include "WinMain.hpp"
 #include "LibDG.hpp"
 #include "ResourceNameHash.hpp"
+#include "Menu.hpp"
 
 #define MAP_IMPL true
+
+struct Light
+{
+    short field_0_r;
+    short field_2_g;
+    short field_4_b;
+    short field_6_a;
+};
+MGS_ASSERT_SIZEOF(Light, 0x8);
+
+struct LitHeader
+{
+    DWORD field_0_num_lights;
+};
+MGS_ASSERT_SIZEOF(LitHeader, 0x4);
 
 struct map_record
 {
@@ -15,7 +31,7 @@ struct map_record
     WORD field_4_map_num;
     WORD field_6_bUsed;
     void* field_8_hzd;
-    DWORD field_C_l_file;
+    LitHeader* field_C_l_file;
     DWORD field_10_script_z_param_bit_index_zone_id;
 };
 MGS_ASSERT_SIZEOF(map_record, 0x14);
@@ -155,15 +171,13 @@ int CC Map_ScriptReloadMaps_44F75E(int zeroOrOne)
 MGS_FUNC_IMPLEX(0x44F75E, Map_ScriptReloadMaps_44F75E, MAP_IMPL);
 
 MGS_FUNC_NOT_IMPL(0x44F5AF, void* CC(int resource_name_hashed, int default_0, __int16 bitIndex, int default_48, int default_24), Map_HZD_Load_44F5AF)
-MGS_FUNC_NOT_IMPL(0x44F53B, int CC(int resourceNameHashed, map_record *pMapStruct), Map_LitLoad_44F53B)
 
 void CC Map_LoadMapData_44F66F(int mapNum, int resourceNameHashed)
 {
     printf("set map %d\n", mapNum);
     map_record* pMap = Map_GetNextFreeRecord_44F505(static_cast<short>(mapNum));
-    pMap->field_8_hzd = Map_HZD_Load_44F5AF(resourceNameHashed, 0, pMap->field_0_map_index_bit, 48, 24);
-    DWORD fileNameHashed = HashFileName_40A58B(resourceNameHashed, 'l');
-    pMap->field_C_l_file = (DWORD)LibGV_FindFile_40A603(fileNameHashed);
+    pMap->field_8_hzd = Map_HZD_Load_44F5AF(resourceNameHashed, 0, static_cast<short>(pMap->field_0_map_index_bit), 48, 24);
+    pMap->field_C_l_file = reinterpret_cast<LitHeader*>(LibGV_FindFile_40A603(HashFileName_40A58B(resourceNameHashed, 'l')));
     Map_LitLoad_44F53B(resourceNameHashed, pMap);
 }
 MGS_FUNC_IMPLEX(0x44F66F, Map_LoadMapData_44F66F, MAP_IMPL);
@@ -288,3 +302,29 @@ void CC Map_Reshade_all_44F8C3()
     }
 }
 MGS_FUNC_IMPLEX(0x44F8C3, Map_Reshade_all_44F8C3, MAP_IMPL);
+
+
+MGS_FUNC_NOT_IMPL(0x443A4E, signed int CC (Prim_unknown_0x48 *pObj, Light* pLights, int lightCount), PrimObjRelated_443A4E);
+
+
+void CC Map_LitLoad_44F53B(int resourceNameHashed, const map_record* pMapStruct)
+{
+    KmdHeader* pFileData = (KmdHeader *)LibGV_FindFile_40A603(HashFileName_40A58B(resourceNameHashed, 'k'));
+    Prim_Union* pPrim = Obj_Alloc_443FEC(pFileData, 87, 0);
+    PsxSetRotationAndTranslation_407A8F(&gIdentity_matrix_6501F8);
+    PsxGetRotationAndTranslation_407BC1(&pPrim->prim_48.field_0_matrix);
+    LitHeader* p_l_file = pMapStruct->field_C_l_file;
+    if (p_l_file)
+    {
+        PrimObjRelated_443A4E(&pPrim->prim_48, DataAfterStructure<Light*>(p_l_file), p_l_file->field_0_num_lights);
+    }
+    else
+    {
+        PrimObjRelated_443A4E(&pPrim->prim_48, nullptr, 0);
+    }
+    Object_Add_40178F(pPrim);
+    pPrim->prim_48.field_2C_index = static_cast<WORD>(pMapStruct->field_0_map_index_bit);
+    gMapOrKmd_Array_dword_7229B8[gMapOrKmdCount_dword_722868] = &pPrim->prim_48;
+    gMapOrKmdCount_dword_722868++;
+}
+MGS_FUNC_IMPLEX(0x44F53B, Map_LitLoad_44F53B, MAP_IMPL);
--- Source/MgsLib/Map.hpp
+++ Source/MgsLib/Map.hpp
@@ -4,3 +4,6 @@ signed int CC Script_tbl_map_45151D(BYTE* pScript);
 void CC Map_FreeHzdItems_44F38D();
 void CC Map_ResetMapCountAndKmdsCount_44F3F7();
 void CC Map_FreeKmds_44F3B5();
+
+struct map_record;
+void CC Map_LitLoad_44F53B(int resourceNameHashed, const map_record* pMapStruct);
