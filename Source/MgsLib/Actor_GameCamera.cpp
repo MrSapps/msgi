e84547f7f50942d800b77ac608384b3f54e31ae9
Paul <Paul@paul.com>
2018-05-27 15:58:09 +0100

    comment the effects of changing some cam vars

--- Source/MgsLib/Actor_GameCamera.cpp
+++ Source/MgsLib/Actor_GameCamera.cpp
@@ -57,18 +57,41 @@ void CC Res_game_camera_create_453B11()
     gCamera_Bits_dword_724864 = 0;
     gCamera_Track_mery172_dword_993FFC = 10000;
     gCamera_Track_dword_993F78 = 10000;
+
+    // Which camera from the HZD map is in use
     gCamera_dword_993FD0 = 0;
+
+    // Distance to follow subject at
     gCamera_unk_993FA0.field_18_track = gCamera_Track_dword_66B718;
+
+    // 3rd person camera pos?
+    // x = up/down bound to the room
+    // y = left/right bound to the room
+    // z = no effect
     gCamera_unk_993FA0.field_10_vec = gCamera_66B720;
+
+    // Causes over the shoulder camera enabled ?
     gCamera_word_994002 = 0;
+
+    // Ditto but further away over the shoulder camera?
     gCamera_dword_724898 = 0;
+
     gCameraFlags_dword_993FF8 = 0;
     gCamera_dword_72489C = 0;
     gCamera_99400C.field_6_padding = 0;
+
+    // FPS camera pos?
+    // x = up/down
+    // y = left/right
+    // z = no effect
     gCamera_unk_993FA0.field_28_vec.field_0_x = 0;
     gCamera_unk_993FA0.field_28_vec.field_4_z = 0;
-    gCamera_zoom_level_word_994000 = 320;
-    gCamera_zoom_copy_dword_993F7C = 320;
     gCamera_unk_993FA0.field_28_vec.field_2_y = 2048;
+
+    // "Normal" zoom level
+    gCamera_zoom_level_word_994000 = 320;
+
+    // Zoom level for camera when menu is open?
+    gCamera_zoom_copy_dword_993F7C = 320;
 }
 MGS_FUNC_IMPLEX(0x453B11, Res_game_camera_create_453B11, GAME_CAMERA_IMPL);
